29cb106503e4e2104fc6a35a85803441eacdcafd
Anthony Grippi <anthonyjgrippi@gmail.com>
2025-04-28 23:52:59 -0700

    fixed authentication by moving amplify config to page instead of layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import "@/lib/amplifyClient";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import "@/lib/amplifyClient";
 import DocumentSelector from "./document-selector/page";
-import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 export default function Home() {
