b7a10ecf60876b1e90fe6da7a413aabd1497d7f2
Titantechs2.0 <titantechs454@gmail.com>
2025-05-07 13:15:00 -0700

    changed dir path for amp outputs

--- app/xxpage.tsx
+++ app/xxpage.tsx
@@ -5,7 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "@/amplify/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
 
