3ca3e89c7522893c39fbbe5fe70c3a9b43c1b89c
napalashe <irunatbears@gmail.com>
2025-05-07 18:15:30 -0700

    refactor: remove unnecessary components of guard duty

--- infra/terraform_security/main.tf
+++ infra/terraform_security/main.tf
@@ -46,10 +46,80 @@ resource "aws_wafv2_web_acl" "amplify_acl" {
       sampled_requests_enabled   = true
     }
   }
+
+  rule {
+    name     = "AWS-AWSManagedRulesSQLiRuleSet"
+    priority = 2
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    override_action {
+      none {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "sqli-rule"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "RateLimit"
+    priority = 3
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "rate-limit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWS-AWSManagedRulesAmazonIpReputationList"
+    priority = 4
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+
+    override_action {
+      none {}
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "ip-reputation"
+      sampled_requests_enabled   = true
+    }
+  }
 }
 
-
-data "aws_amplify_app" "app" {
-    #Enter aomplify id
-  app_id = "<YOUR_AMPLIFY_APP_ID>"
-}
+# AWS Shield Advanced
+# resource "aws_shield_protection" "amplify_protection" {
+#   name         = "amplify-shield-protection"
+#   resource_arn = "<YOUR_AMPLIFY_APP_ARN>"
+#
+#   tags = {
+#     Environment = "Production"
+#   }
+# }
